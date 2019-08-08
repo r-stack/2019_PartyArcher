d278cee99e48c29c9b20adae9f1a598a7cb59f4f
岩崎信也 / IWASAKI，SHINYA <shinya.iwasaki.fb@hitachi-systems.com>
2019-08-08 18:10:54 +0900

    余分な文字があったので修正＋フォーマット

--- src/pages/Gacha.vue
+++ src/pages/Gacha.vue
@@ -1,100 +1,111 @@
 <template>
   <main-layout>
     <div class="content">
-      <div class="gachagachas" v-show="!result" v-bind:class="{ smile: isActive }" v-on:animationend="next(item)"></div>
-      <div class="gacha_result_present" v-show="result_present"  >
-      </div>
-      <div class="gacha_result" v-show="result_result"  >
-        <p class="b_text"> おめでとう<br>{{ gacha.gain }} P<br>を手に入れた</p>
+      <div
+        class="gachagachas"
+        v-show="!result"
+        v-bind:class="{ smile: isActive }"
+        v-on:animationend="next(item)"
+      ></div>
+      <div class="gacha_result_present" v-show="result_present"></div>
+      <div class="gacha_result" v-show="result_result">
+        <p class="b_text">
+          おめでとう
+          <br />
+          {{ gacha.gain }} P
+          <br />を手に入れた
+        </p>
       </div>
     </div>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
-  import axios from 'axios'
+import MainLayout from "../layouts/Main.vue";
+import axios from "axios";
 
-  export default {
-    components: {
-      MainLayout
+export default {
+  components: {
+    MainLayout
+  },
+  data: function() {
+    return {
+      result: false,
+      result_result: false,
+      result_present: false,
+      isActive: false,
+      hoges: "test11",
+      gacha: "test111"
+    };
+  },
+  methods: {
+    start: function() {
+      this.$data.isActive = !this.$data.isActive;
     },
-    data: function() {
-      return {
-        result: false,
-        result_result: false,
-        result_present: false,
-        isActive: false,
-        hoges: "test11",
-        gacha: "test111"
+    next: function() {
+      this.$data.result = !this.$data.result;
+      this.$data.result_result = !this.$data.result_result;
+      console.log(this.$data.gacha);
+      if (this.$data.gacha.reward) {
+        setTimeout(() => {
+          this.$data.result = true;
+          this.$data.result_result = false;
+          this.$data.result_present = true;
+        }, 3000);
       }
-    },
-    methods: {
-      start: function () {
-        this.$data.isActive = !this.$data.isActive
-      },
-      next: function(){
-        this.$data.result = !this.$data.result;
-        this.$data.result_result = !this.$data.result_result;
-        console.log(this.$data.gacha)
-        if(this.$data.gacha.reward){
-          setTimeout(() => {
-            this.$data.result=true;
-            this.$data.result_result=false;
-            this.$data.result_present  = true; 
-          }, 3000);
-        }
-      }
-    },
-    mounted: function () {
-      axios
-      .get('https://b820fpmdz4.execute-api.ap-northeast-1.amazonaws.com/dev/TeamArcher-Gacha')
-      .then(response => (this.gacha = response.data));
-      this.start();
     }
+  },
+  mounted: function() {
+    axios
+      .get(
+        "https://b820fpmdz4.execute-api.ap-northeast-1.amazonaws.com/dev/TeamArcher-Gacha"
+      )
+      .then(response => (this.gacha = response.data));
+    this.start();
   }
+};
 </script>
 
 <style scoped>
-  .content {
-      height: 100%;
-      margin: 0px;
-      width: 100%; 
+.content {
+  height: 100%;
+  margin: 0px;
+  width: 100%;
+}
+.gacha_result {
+  position: relative;
+  background-image: url("/img/リザルト.png");
+  background-size: 100%;
+  width: 100%;
+  height: 100%;
+}
+.gacha_result_present {
+  position: relative;
+  background-image: url("/img/荷物.png");
+  background-size: 100%;
+  width: 100%;
+  height: 100%;
+}
+.gachagachas {
+  position: relative;
+  background-image: url("/img/gatya.png");
+  background-size: cover;
+  height: 100%;
+}
+.b_text {
+  font-size: 60px;
+  color: white;
+  z-index: 50;
+  position: absolute;
+  left: 60px;
+  top: 200px;
+}
+.smile {
+  animation: animationSmile 2s steps(30) 1;
+}
+@keyframes animationSmile {
+  to {
+    background-position: -12330px 0;
   }
-  .gacha_result{
-    position:relative; 
-    background-image: url("/img/リザルト.png");
-    background-size: 100%;
-    width: 100%; 
-    height: 100%;
-  }
-  .gacha_result_present{
-    position:relative; 
-    background-image: url("/img/荷物.png");
-    background-size: 100%;
-    width: 100%; 
-    height: 100%;
-  }
-  .gachagachas {
-    position:relative; 
-    background-image: url("/img/gatya.png");
-    background-size: cover;
-    height: 100%;
-  }
-  .b_text {
-    font-size: 60px;
-    color: white;
-    z-index: 50;
-  		position: absolute;  
-      left: 60px;
-        top: 200px;dd
-  }
-  .smile {
-    animation: animationSmile 2s steps(30) 1;
-  }
-  @keyframes animationSmile {
-    to {
-      background-position: -12330px 0;
-    }
-  }
+}
 </style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,99 +1,114 @@
 <template>
   <main-layout>
     <div class="content">
-      <div style="background-image: url(img/背景.png); background-repeat: no-repeat; background-size: cover; height:100%; width:100%;">
+      <div
+        style="background-image: url(img/背景.png); background-repeat: no-repeat; background-size: cover; height:100%; width:100%;"
+      >
         <p class="wallet">{{rs.wallet}}</p>
-        <img src="img/chokin_frame.png"  class="content1" width="220" height="50" alt="">
-        <img class="meter" src="img/chokin_meter.png" v-bind:style="{ width: computedWidth + 'px' }" height="50" alt="">
-        <img src="img/saihu.png"  class="content2" width="191" height="100" alt="">
-        <v-link href="/gacha"><img src="img/gacha.png" width="150" height="150" alt=""  class="content3"></v-link>
+        <img src="img/chokin_frame.png" class="content1" width="220" height="50" alt />
+        <img
+          class="meter"
+          src="img/chokin_meter.png"
+          v-bind:style="{ width: computedWidth + 'px' }"
+          height="50"
+          alt
+        />
+        <img src="img/saihu.png" class="content2" width="191" height="100" alt />
+        <v-link href="/gacha">
+          <img src="img/gacha.png" width="150" height="150" alt class="content3" />
+        </v-link>
       </div>
     </div>
   </main-layout>
 </template>
 
 <script>
-  import MainLayout from '../layouts/Main.vue'
-  import axios from 'axios'
-  import VLink from '../components/VLink.vue'
+import MainLayout from "../layouts/Main.vue";
+import axios from "axios";
+import VLink from "../components/VLink.vue";
 
-  export default {
-    components: {
-      MainLayout,
-      VLink
-    },
-    data: function() {
-      return {
-        result: false,
-        computedWidth: "30",
-        isActive: false,
-        hoges: "test11",
-        rs: {
-          "wallet":"500",
-          "savings":"300",
-          "goal":"100"
-        }
+export default {
+  components: {
+    MainLayout,
+    VLink
+  },
+  data: function() {
+    return {
+      result: false,
+      computedWidth: "30",
+      isActive: false,
+      hoges: "test11",
+      rs: {
+        wallet: "500",
+        savings: "300",
+        goal: "100"
       }
-    },
-    methods: {
-      start: function () {
-        console.log("start")
-        console.log(this.$data.rs)
-        this.$data.computedWidth = Number((Number(165) * Number(this.$data.rs.savings) / this.$data.rs.goal));
-        console.log(this.$data.computedWidth)
-        
-      }
-    },
-    mounted: function () {
-      axios
-      .get('https://b820fpmdz4.execute-api.ap-northeast-1.amazonaws.com/dev/TeamArcher-GetStatus')
-      .then(function(response){
-        this.rs = response.data;
-        this.start();
-      }.bind(this))
+    };
+  },
+  methods: {
+    start: function() {
+      console.log("start");
+      console.log(this.$data.rs);
+      this.$data.computedWidth = Number(
+        (Number(165) * Number(this.$data.rs.savings)) / this.$data.rs.goal
+      );
+      console.log(this.$data.computedWidth);
     }
+  },
+  mounted: function() {
+    axios
+      .get(
+        "https://b820fpmdz4.execute-api.ap-northeast-1.amazonaws.com/dev/TeamArcher-GetStatus"
+      )
+      .then(
+        function(response) {
+          this.rs = response.data;
+          this.start();
+        }.bind(this)
+      );
   }
+};
 </script>
 
 <style scoped>
-  .content {
-      position:relative;
-      height: 100%;
-      margin: 0px;
-      width: 100%; 
-  }
-  .content1 {
-    position:absolute; 
-    left: 4px;
-    top:20px
-  }
-  .wallet {
-    font-size: 30px;
-    z-index: 50;
-    position:absolute; 
-    right: 40px;
-    top: 26px;
-  }
-  .meter {
-    z-index: 50;
-    position:absolute; 
-    left: 5px;
-    top: 20px;
-    mater: 10px;
-    object-fit: none;
-    object-position: left;
-  }
+.content {
+  position: relative;
+  height: 100%;
+  margin: 0px;
+  width: 100%;
+}
+.content1 {
+  position: absolute;
+  left: 4px;
+  top: 20px;
+}
+.wallet {
+  font-size: 30px;
+  z-index: 50;
+  position: absolute;
+  right: 40px;
+  top: 26px;
+}
+.meter {
+  z-index: 50;
+  position: absolute;
+  left: 5px;
+  top: 20px;
+  mater: 10px;
+  object-fit: none;
+  object-position: left;
+}
 
-  .content2 {
-    z-index: 20;
-    position:absolute; 
-    top: 0px;
-    left: 215px;
-  }
+.content2 {
+  z-index: 20;
+  position: absolute;
+  top: 0px;
+  left: 215px;
+}
 
-  .content3 {
-    position:absolute; 
-    top: 570px; 
-    left: 250px;
-  }
+.content3 {
+  position: absolute;
+  top: 570px;
+  left: 250px;
+}
 </style>
